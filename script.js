b3315f6ce6f2f038b03f51675d3843805132bfb8
renukaketlary <renukaketlary@gmail.com>
2025-11-14 21:39:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const hero = document.querySelector('.hero1');
 
   // Array of background images
-  /*const images = [
+  const images = [
     'url("https://th.bing.com/th/id/OIP.sI-TElAvDKohsbmZo8gmaQHaEJ?w=312&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")',
     'url("https://th.bing.com/th/id/OIP.JPogPMSOOv8g9WJdtLUluAHaEe?w=240&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")',
     'url("https://th.bing.com/th/id/OIP.S16MwWFznloeMQVbOE-bugHaEp?w=229&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")'
   ];
-  */
+  
   let index = 0;
 
   setInterval(() => {
